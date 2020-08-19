7372532c0553f51162f33889236055ed0d68ad1e
dimitroffangel <angeldimitrov05@gmail.com>
2020-08-19 04:08:19 +0300

    added UnitTests::Allocate_Via_MallocFree_MediumObjects()

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -1090,5 +1090,39 @@ void UnitTests::Allocate_Via_MallocFree_SmallObjects_Add_Delete()
 
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << '\n';
+}
 
+void UnitTests::Allocate_Via_MallocFree_MediumObjects()
+{
+	std::cout << "UnitTests:: Allocate_Via_MallocFree_MediumObjects(): ";
+
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	const int blocks = 4;
+	const size_t sizeOfPtrInt = sizeof(PtrInt);
+	const size_t sizeOfFoo = sizeof(Foo);
+	const size_t sizeOfEpicFoo = sizeof(EpicFoo);
+	const size_t sizeofGiantFoo = sizeof(GiantFoo);
+
+	const int size = 10000;
+
+	std::vector<EpicFoo*> foos;
+	foos.reserve(size);
+
+	for (size_t i = 0; i < size; i++)
+	{
+		void* rawPointer = malloc(sizeof(EpicFoo));
+		EpicFoo* result1 = new (rawPointer) (EpicFoo);
+		foos.push_back(result1);
+	}
+
+
+	for (size_t i = 0; i < size; i++)
+	{
+		foos[i]->~EpicFoo();
+		free(foos[i]);
+	}
+
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << '\n';
 }
