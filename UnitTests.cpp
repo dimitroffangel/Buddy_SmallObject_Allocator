5bef067339f558ac4d96324151c36cef98383b47
dimitroffangel <angeldimitrov05@gmail.com>
2020-08-19 04:19:11 +0300

    added UnitTests::Allocate_Via_MallocFree_BigObjects_Add_Delete()

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -1191,3 +1191,35 @@ void UnitTests::Allocate_Via_MallocFree_BigObjects()
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << '\n';
 }
+
+void UnitTests::Allocate_Via_MallocFree_BigObjects_Add_Delete()
+{
+	std::cout << "UnitTests::Allocate_Via_MallocFree_BigObjects_Add_Delete(): ";
+
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	const int blocks = 4;
+	const size_t sizeOfPtrInt = sizeof(PtrInt);
+	const size_t sizeOfFoo = sizeof(Foo);
+	const size_t sizeOfEpicFoo = sizeof(EpicFoo);
+	const size_t sizeofGiantFoo = sizeof(GiantFoo);
+
+	const int size = 10000;
+
+	std::vector<GiantFoo*> foos;
+	foos.reserve(size);
+
+	for (size_t i = 0; i < size; i++)
+	{
+		void* rawPointer = malloc(sizeof(GiantFoo));
+		GiantFoo* result1 = new (rawPointer) (GiantFoo);
+		foos.push_back(result1);
+
+		foos[i]->~GiantFoo();
+		free(foos[i]);
+	}
+
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << '\n';
+
+}
